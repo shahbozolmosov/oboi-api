c5311b447e310a347560c876dba5a6a83d1d807c
shahboz007 <olmasovshahboz007@gmail.com>
2022-10-17 11:27:40 +0500

    add api param validation

--- api/user/index.php
+++ api/user/index.php
@@ -1,27 +1,72 @@
 <?php
-require "../../config/Database.php";
-require "../../modules/User.php";
+// headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+require_once "../../config/Database.php";
+require_once "../../modules/User.php";
+require_once "../../modules/CheckToken.php";
 
 // Instantiate DB & connect
 $database = new Database();
 $db = $database->connect();
 
+// Check token
+$requestHeaders = apache_request_headers();
+$Authorization = $database->filter($requestHeaders['Authorization']);
+$checkToken = new CheckToken($db);
+$result = $checkToken->check($Authorization);
+if (!$result) {
+    http_response_code(400);
+    print(json_encode(['message' => 'Bad Requestaa!']));
+    exit;
+}
 
 //Instantiate user
 $user = new User($db);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' || true) {
-    $user->telefon = $database->filterPhoneNumber('+998(99)-747-04-73');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // Check user action
-    $result = $user->checkUserActions();
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+    // Validation action
+    validation($data, ['action']);
 
-    if ($result === 'ok') {
+    if ($data->action === 'register') {
+        // validation telefon
+        validation($data, ['telefon']);
+
+        $user->telefon = $database->filterPhoneNumber($data->telefon);
+
         $result = $user->register();
-        echo "success";
-    } else {
+
         http_response_code($result['status_code']);
         print(json_encode($result['data']));
         exit;
+    } else if ($data->action === 'verification') {
+        echo "Verification";
+        exit();
     }
 }
+
+// VALIDATION
+function validation($data, $checkParam)
+{
+    foreach ($checkParam as $key => $value) {
+        $paramValue = $data->{$value};
+        if ($paramValue === null) {
+            http_response_code(400);
+            print(json_encode('`' . $value . '` talab qilinadi!')) ;
+            exit();
+        }else if($paramValue === '') {
+            http_response_code(400);
+            print(json_encode('`' . $value . '` bo\'sh qator bo\'lmasin!')) ;
+            exit();
+        }
+    }
+}
+
+http_response_code(400);
+print(json_encode(['message' => 'Bad Request!']));
