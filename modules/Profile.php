736af34197b2ff5dcdf3cf78faffb28e818e72cc
shahboz007 <olmasovshahboz007@gmail.com>
2022-10-21 08:53:55 +0500

    done profile

--- modules/Profile.php
+++ modules/Profile.php
@@ -27,27 +27,41 @@ class Profile extends User
     // Change table
     $this->table = 'clients';
 
-    $query = 'SELECT id FROM ' . $this->table . ' WHERE token=:token';
+    $query = 'SELECT id, balans FROM ' . $this->table . ' WHERE token=:token';
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(':token', $this->token);
     $stmt->execute();
 
     $userData = $stmt->fetch(PDO::FETCH_ASSOC);
     $userId = $userData['id'];
-
+    $userBalans = $userData['balans'];
+    
     if ($userData) {
       // Change table
       $this->table = 'order_clients';
-      $query = 'SELECT * FROM order_clients WHERE client_id=:userId';
+      $query = 'SELECT * FROM order_clients WHERE client_id=:userId  ORDER BY id DESC';
       $stmt = $this->conn->prepare($query);
       $stmt->bindParam(':userId', $userId);
       $stmt->execute();
-      $orderData = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($orderData) {
+      $orderData = [];
+      while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $newRow = [
+          'id' => $row['id'],
+          'article' => $row['article'],
+          'phone' => $row['phone'],
+          'soni' => $row['soni'],
+          'manzil' => $row['manzil'],
+          'sana' => date('d-m-Y', $row['sana']),
+          'status' => $row['status'],
+          'cashback' => $row['cashback'],
+        ];
+        $orderData['orders'][] = $newRow;
+        $orderData['balans'] = $userBalans;
+      }
+      
+      if($orderData['orders']) {
         return [
-          'data' => [
-            'orders' => $orderData
-          ],
+          'data' => $orderData,
           'status_code' => 200
         ];
       }
